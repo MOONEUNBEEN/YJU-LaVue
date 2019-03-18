d9089e07344fef2ac9b4e6717b9b8092b76d4374
MOONEUNBEEN <e_bb@daum.net>
2019-03-18 15:50:00 +0900

    laravel + vue

---
+++ Aromind/front/src/components/product/Product.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <img src="https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png" alt="">
+
+        <div class="caption">
+          <h3>{{ product.name }}</h3>
+          <p>{{ product.price }}</p>
+
+          <hr>
+
+          <p>
+            <a href="" class="btn btn-default">
+              Wish list
+            </a>
+
+            <a href="" class="btn btn-success">
+              Buy
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    props: ['product']
+  }
+</script>
---
+++ Aromind/front/src/routes.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Login from './components/authentication/Login.vue'
+import Register from './components/authentication/Register.vue'
+import Feed from './components/Feed.vue'
+import CreateProduct from './components/product/Create.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: "/login",
+      component: Login,
+      meta: {
+        forVisitors: true
+      }
+    },
+
+    {
+      path: "/register",
+      component: Register,
+      meta: {
+        forVisitors: true
+      }
+    },
+
+    {
+      path: "/feed",
+      component: Feed,
+      meta: {
+        forAuth: true
+      }
+    },
+
+    {
+      path: "/products/create",
+      component: CreateProduct,
+      meta: {
+        forAuth: true
+      }
+    }
+  ],
+
+  linkActiveClass: 'active'
+})
+
+export default router
